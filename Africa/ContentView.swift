13d8d27592cb89acafb5f445a06148b10038bbb3
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-09-27 07:11:19 -0400

    Initial Commit

---
+++ Africa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Jimmy Ghelani on 2023-09-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
